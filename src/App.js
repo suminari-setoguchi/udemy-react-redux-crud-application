1f53dd1a3f4fd17009dcb375a3d4524f1840a039
瀬戸口純也 <junya.setoguchi@pixta.co.jp>
2020-06-13 23:33:04 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hanako", age: 5 },
-    { name: "Nobody"},
+    { name: "Nobody", age: 3},
   ];
   return (
     <div className="">
@@ -27,8 +28,10 @@ const User = (props) => {
   )
 }
 
-User.defaultProps ={
-  age: 1
-}
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
+};
+
 
 export default App;
